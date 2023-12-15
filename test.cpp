60a3ca61644be1b7bc71e6c4fbf2406adc7d0d3e
burlica1234 <152985864+burlica1234@users.noreply.github.com>
2023-12-15 09:59:01 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,16 +8,11 @@
 using namespace std;
 
 int tabla[MAX][MAX];
-int n; 
+int n;
 int PiesaAleasa;
+int lv, cv, la, ca, ln, cn;
+int piesaAleasa;
 
-void afiseazaMeniul()
-{
-	readimagefile("1.jpg", 50, 25, 100, 75);
-	readimagefile("2.jpg", 150, 25, 200, 75);
-	readimagefile("3.jpg", 250, 25, 300, 75);
-	readimagefile("0.jpg", 350, 25, 400, 75);
-}
 void initTabla()
 {
 	n = 8;
@@ -43,7 +38,7 @@ void desPiesa(int p, int l, int c)
 	case 1:
 		strcpy(numeFisier, "1.jpg");
 		break;
-	case 2 :
+	case 2:
 		strcpy(numeFisier, "2.jpg");
 		break;
 	case 3:
@@ -73,7 +68,148 @@ void desTabla()
 		}
 	}
 }
+void alegePiesa(int& p)
+{
+	int x, y;
+	p = 0;
+	if (ismouseclick(WM_LBUTTONDOWN))
+	{
+		clearmouseclick(WM_LBUTTONDOWN);
+		x = mousex();
+		y = mousey();
+		if (x >= 50 && x <= 100 && y >= 25 && y <= 75) p = 1;
+		if (x >= 150 && x <= 200 && y >= 25 && y <= 75) p = 2;
+		if (x >= 250 && x <= 300 && y >= 25 && y <= 75) p = 3;
+		if (x >= 350 && x <= 400 && y >= 25 && y <= 75) p = 4;
+		Beep(1000, 200);
+	}
+}
+bool vinDinStanga()
+{
+	return lv == la && cv == ca - 1;
+}
 
+bool vinDeSus()
+{
+	return lv == la - 1 && cv == ca;
+}
+
+bool vinDinDreapta()
+{
+	return lv == la && cv == ca + 1;
+}
+
+bool vinDeJos()
+{
+	return lv == la + 1 && cv == ca;
+}
+
+void mergJos()
+{
+	cn = ca;
+	ln = la + 1;
+}
+
+void mergSus()
+{
+	cn = ca;
+	ln = la - 1;
+}
+
+void mergStanga()
+{
+	cn = ca - 1;
+	ln = la;
+}
+
+void mergDreapta()
+{
+	cn = ca + 1;
+	ln = la;
+}
+void calculeazaLoculUndeTrebuiePusaPiesa()
+{
+	do
+	{
+		switch (tabla[la][ca])
+		{
+			/* piesa 1 = \\ */
+		case 1:
+			if (vinDinStanga()) mergJos();
+			else if (vinDinDreapta()) mergSus();
+			else if (vinDeJos()) mergStanga();
+			else mergDreapta();
+			break;
+			/* piesa 2 = // */
+		case 2:
+			if (vinDinStanga()) mergSus();
+			else if (vinDinDreapta()) mergJos();
+			else if (vinDeJos()) mergDreapta();
+			else mergStanga();
+			break;
+			/* piesa 3 = + */
+		case 3:
+			if (vinDinStanga()) mergDreapta();
+			else if (vinDinDreapta()) mergStanga();
+			else if (vinDeJos()) mergSus();
+			else mergJos();
+			break;
+		}
+		// cout<<"     lv="<<lv<<" cv="<<cv<<", la="<<la<<" ca="<<ca<<" -> ln="<<ln<<" cn="<<cn<<endl;
+		lv = la; cv = ca;
+		la = ln; ca = cn;
+	} while (tabla[ln][cn] != 0);
+}
+
+int punerePiesa()
+{
+		int linia, coloana, x, y;
+		int stanga = 50, sus = 150;
+		int dreapta = 50 + 50 * n, jos = 150 + 50 * n;
+		bool ok = false;
+		bool alegere_corecta;
+		int lv1 = lv, cv1 = cv, la1 = la, ca1 = ca;
+		do
+		{
+			alegere_corecta = false;
+			if (ismouseclick(WM_LBUTTONDOWN))
+			{
+				clearmouseclick(WM_LBUTTONDOWN);
+				x = mousex();
+				y = mousey();
+				if (x >= stanga && x <= dreapta && y >= sus && y <= jos)
+				{
+					linia = (y - sus) / 50 + 1;
+					coloana = (x - stanga) / 50 + 1;
+					calculeazaLoculUndeTrebuiePusaPiesa(); // determina ln,cn
+					alegere_corecta = tabla[linia][coloana] == 0 && linia == ln && coloana == cn;
+					// cout<<"lv="<<lv<<" cv="<<cv<<", la="<<la<<" ca="<<ca<<" -> ln="<<ln<<" cn="<<cn<<endl;
+					if (alegere_corecta)
+					{
+						Beep(1500, 200);
+						tabla[linia][coloana] = piesaAleasa;
+						desPiesa(piesaAleasa, linia, coloana);
+					}
+					else {
+						Beep(200, 200);
+						lv = lv1; cv = cv1; la = la1; ca = ca1;
+					}
+				}
+			}
+		} while (!alegere_corecta);
+		return ok;
+}
+	bool jocFinal()
+	{
+		return ln == 0 || cn == 0;
+	}
+void afiseazaMeniul()
+{
+		readimagefile("1.jpg", 50, 25, 100, 75);
+		readimagefile("2.jpg", 150, 25, 200, 75);
+		readimagefile("3.jpg", 250, 25, 300, 75);
+		readimagefile("0.jpg", 350, 25, 400, 75);
+}
 int main()
 {
 
@@ -85,8 +221,21 @@ int main()
 	afiseazaMeniul();
 	initTabla();
 	desTabla();
+	do
+	{
+		piesaAleasa = 0;
+		do
+		{
+			alegePiesa(piesaAleasa);
+			if (piesaAleasa == 4)
+			{
+				closegraph();
+				return 0;
+			}
+		} while (piesaAleasa == 0);
+		punerePiesa();
+	} while (!jocFinal());
 	getch();
 	closegraph();
 	return 0;
 }
-
